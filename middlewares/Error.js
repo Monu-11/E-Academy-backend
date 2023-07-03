c988f9c619c3955f53edbfcebce615ea65959332
Monu-11 <monukumarsinghmonu@gmail.com>
2023-07-03 15:41:48 +0530

    Error Middleware Issue Resolved

--- middlewares/Error.js
+++ middlewares/Error.js
@@ -1,7 +1,8 @@
 const ErrorMiddleware = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal Server Error";
-  res.status(res.statusCode).json({
+  res.status(err.statusCode).json({
+    // Update this line
     success: false,
     message: err.message,
   });
