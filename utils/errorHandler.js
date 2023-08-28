f6aa9940453ebf0d079b719577c9cf6e4daadee6
Monu-11 <monukumarsinghmonu@gmail.com>
2023-08-28 21:56:26 +0530

    change class component to function component

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,7 +1,16 @@
-class ErrorHandler extends Error {
-  constructor(message, statusCode) {
-    super(message);
-    this.statusCode = statusCode;
-  }
+// class ErrorHandler extends Error {
+//   constructor(message, statusCode) {
+//     super(message);
+//     this.statusCode = statusCode;
+//   }
+// }
+// export default ErrorHandler;
+
+//function components
+function ErrorHandler(message, statusCode) {
+  const error = new Error(message);
+  error.statusCode = statusCode;
+  return error;
 }
+
 export default ErrorHandler;
